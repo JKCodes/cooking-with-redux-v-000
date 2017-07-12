b682d3522dde4620c22cababd7455d3a10cdf4c4
Joseph Kim <josephkim0224@gmail.com>
2017-07-12 00:41:49 +0000

    Done.

--- src/components/recipes/RecipesInput.js
+++ src/components/recipes/RecipesInput.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addRecipe } from '../../actions/recipes'
+import { AddIngredients } from '../ingredients/AddIngredients'
 
 export class RecipesInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: ""
+    }
+  }
+
+  handleOnRecipeNameChange(e){
+    this.setState({name: e.target.value})
+  }
+
+  handleOnCaloriesChange(e){
+    this.setState({calories: e.target.value})
+  }
+
+  handleOnSubmit(e){
+    e.preventDefault()
+    let recipe = Object.assign({}, this.state, {ingredientIds: this.props.selectedIngredients})
+    this.props.addRecipe(recipe)
+  }
+
   render(){
     return(
-      <div>
-        Recipes Input
-      </div>
+      <form onSubmit={(e) => this.handleOnSubmit(e)}>
+      <p>
+        <input type="text" onChange={(e) => this.handleOnRecipeNameChange(e)} placeholder="Recipe name"/>
+      </p>
+      <p>
+
+      </p>
+        <AddIngredients />
+        <input type="submit" />
+      </form>
     )
   }
 }
 
-export const ConnectedRecipesInput =(RecipesInput)
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({addRecipe: addRecipe}, dispatch)
+}
+
+function mapStateToProps(state){
+  return { selectedIngredients: state.recipeForm.ingredientIds }
+}
+
+export const ConnectedRecipesInput = connect(mapStateToProps, mapDispatchToProps)(RecipesInput)
